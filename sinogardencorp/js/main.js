f1fc08c7d9102f895b74b292cde5356eceff27e2
Kevin Kuan <kevinkuan4@gmail.com>
2017-11-10 12:27:59 -0500

    changed fading origin to bottom(default)

--- sinogardencorp/js/main.js
+++ sinogardencorp/js/main.js
@@ -3,19 +3,18 @@
         
         sr.reveal('.c1', {
           duration: 2000,
-          origin:'left',
           distance:'300px',
           viewFactor: 0.2
         });
         
         sr.reveal('.c2', {
           duration: 2000,
+            distance: '300px',
           viewFactor: 0.2
         });
         
         sr.reveal('.c3', {
           duration: 2000,
-          origin:'right',
           distance:'300px',
           viewFactor: 0.2
         });
